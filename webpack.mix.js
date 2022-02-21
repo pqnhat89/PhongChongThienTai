3dcd214d798c7acaeb37f6ddff5853d95a8ba7fd
Phan Quang Nhat <nhat.phan@dac-datatech.vn>
2022-02-21 14:34:58 +0700

    update lib

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
-const mix = require('laravel-mix');
+const { mix } = require('laravel-mix');
+
 mix.options({
     uglify: {
         sourceMap: true,
@@ -8,17 +9,6 @@ mix.options({
         }
     }
 });
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
 
 mix.js('resources/js/scripts.js', 'public/js')
     .sass('resources/sass/main.scss', 'public/css')
