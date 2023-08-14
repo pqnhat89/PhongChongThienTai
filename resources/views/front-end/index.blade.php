f2174c919b3bd595d10f9b764fef0909782e9bd4
cuongnq <cuongnq211@gmail.com>
2023-08-14 17:56:48 +0700

    add DBTT, KHPA  posts into home page

--- resources/views/front-end/index.blade.php
+++ resources/views/front-end/index.blade.php
@@ -70,202 +70,116 @@
         </div>
     @else
 	    <?php
-	    $bttt = \App\Services\PostServices::getPostByType(\App\Enums\PostType::BTTT);
-	    $vbcd_dk = \App\Services\PostServices::getPostByType(\App\Enums\PostType::VBCD_CDK);
-	    $ttsk = \App\Services\PostServices::getPostByType(\App\Enums\PostType::TTSK);
-	    $vbcddh = \App\Services\PostServices::getPostByType(\App\Enums\PostType::VBCDDH);
+            $items = [
+                \App\Enums\PostType::TTSK => \App\Services\PostServices::getPostByType(\App\Enums\PostType::TTSK),
+                \App\Enums\PostType::BTTT => \App\Services\PostServices::getPostByType(\App\Enums\PostType::BTTT),
+                \App\Enums\PostType::DBTT => \App\Services\PostServices::getPostByType(\App\Enums\PostType::DBTT),
+                \App\Enums\PostType::VBCD_CDK => \App\Services\PostServices::getPostByType(\App\Enums\PostType::VBCD_CDK),
+                \App\Enums\PostType::VBCDDH => \App\Services\PostServices::getPostByType(\App\Enums\PostType::VBCDDH),
+                \App\Enums\PostType::KHPA => \App\Services\PostServices::getPostByType(\App\Enums\PostType::KHPA),
+            ];
 	    ?>
-        <div>
-            <div>
+        @foreach($items as $cat_name => $cat_list)
+            @if(in_array($cat_name, [\App\Enums\PostType::TTSK, \App\Enums\PostType::BTTT, \App\Enums\PostType::DBTT]))
                 <div>
                     <div>
                         <div>
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title"
-                                        style="font-size: 12px; font-weight: bold">
-                                        {{\App\Enums\PostType::TTSK}}
-                                    </h3>
-                                </div>
-                                <div class="panel-body" style="padding: 0px;">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <a href="{{url('/xem') . '/' . $ttsk[0]->id}}">
-                                                <div class="thumbnail"
-                                                     style="padding: 0px; margin-bottom: 0px">
-                                                    <img src="{{ url($ttsk[0]->image ?? '') }}"
-                                                         alt="100%x200"
-                                                         data-src="holder.js/100%x200"
-                                                         style="width: 100%; display: block;"
-                                                         data-holder-rendered="true">
-                                                    <div class="caption">
-                                                        <p style="font-weight: bold; margin: 0 0 0px; color: #d82525; text-align: justify">
-                                                            {{$ttsk[0]->title}}
-                                                        </p>
-                                                        <p style="text-align: justify; margin-bottom: 0px">
-                                                            {{$ttsk[0]->sub_title}}
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </a>
+                            <div>
+                                <div>
+                                    <div class="panel panel-primary">
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title"
+                                                style="font-size: 12px; font-weight: bold">
+                                                <a href="">{{$cat_name}}</a>
+                                            </h3>
                                         </div>
-                                        <div class="col-md-6" style="padding-left: 0;">
-                                            <ul class="list-group" style="margin-left: 0px">
-                                                @for($i=1; $i<count($ttsk); $i++)
-                                                    <li class="list-group-item"
-                                                        style="padding: 6px 10px;">
-                                                        <a href="{{url('/xem') . '/' . $ttsk[$i]->id}}"
-                                                           style="color: #333">
-                                                            <i class="fa fa-caret-right"
-                                                               aria-hidden="true"></i>
-                                                            {{$ttsk[$i]->title}}
-                                                        </a>
-                                                    </li>
-                                                @endfor
-                                            </ul>
+                                        <div class="panel-body" style="padding: 0px;">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <a href="{{url('/xem') . '/' . $cat_list[0]->id}}">
+                                                        <div class="thumbnail"
+                                                            style="padding: 0px; margin-bottom: 0px">
+                                                            <img src="{{ url($cat_list[0]->image ?? '') }}"
+                                                                alt="100%x200"
+                                                                data-src="holder.js/100%x200"
+                                                                style="width: 100%; display: block;"
+                                                                data-holder-rendered="true">
+                                                            <div class="caption">
+                                                                <p style="font-weight: bold; margin: 0 0 0px; color: #d82525; text-align: justify">
+                                                                    {{$cat_list[0]->title}}
+                                                                </p>
+                                                                <p style="text-align: justify; margin-bottom: 0px">
+                                                                    {{$cat_list[0]->sub_title}}
+                                                                </p>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                                <div class="col-md-6" style="padding-left: 0;">
+                                                    <ul class="list-group" style="margin-left: 0px">
+                                                        @for($i=1; $i<count($cat_list); $i++)
+                                                            <li class="list-group-item"
+                                                                style="padding: 6px 10px;">
+                                                                <a href="{{url('/xem') . '/' . $cat_list[$i]->id}}"
+                                                                style="color: #333">
+                                                                    <i class="fa fa-caret-right"
+                                                                    aria-hidden="true"></i>
+                                                                    {{$cat_list[$i]->title}}
+                                                                </a>
+                                                            </li>
+                                                        @endfor
+                                                    </ul>
+
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <div class="clear"></div>
                     </div>
                 </div>
-                <div class="clear"></div>
-            </div>
-        </div>
-        <div>
-            <div>
+            @else
                 <div>
                     <div>
                         <div>
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title"
-                                        style="font-size: 12px; font-weight: bold">
-                                        <a href="">{{\App\Enums\PostType::BTTT}}</a>
-                                    </h3>
-                                </div>
-                                <div class="panel-body" style="padding: 0px;">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <a href="{{url('/xem') . '/' . $bttt[0]->id}}">
-                                                <div class="thumbnail"
-                                                     style="padding: 0px; margin-bottom: 0px">
-                                                    <img src="{{ url($bttt[0]->image ?? '') }}"
-                                                         alt="100%x200"
-                                                         data-src="holder.js/100%x200"
-                                                         style="width: 100%; display: block;"
-                                                         data-holder-rendered="true">
-                                                    <div class="caption">
-                                                        <p style="font-weight: bold; margin: 0 0 0px; color: #d82525; text-align: justify">
-                                                            {{$bttt[0]->title}}
-                                                        </p>
-                                                        <p style="text-align: justify; margin-bottom: 0px">
-                                                            {{$bttt[0]->sub_title}}
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </a>
+                            <div>
+                                <div>
+                                    <div class="panel panel-primary">
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title"
+                                                style="font-size: 12px; font-weight: bold">
+                                                <a href="">{{$cat_name}}</a>
+                                            </h3>
                                         </div>
-                                        <div class="col-md-6" style="padding-left: 0;">
-                                            <ul class="list-group" style="margin-left: 0px">
-                                                @for($i=1; $i<count($bttt); $i++)
+                                        <div class="panel-body" style="padding: 0px;">
+
+                                            <ul class="list-group"
+                                                style="margin-left: 0px; margin-bottom: 0px;">
+                                                @foreach($cat_list as $list)
                                                     <li class="list-group-item"
-                                                        style="padding: 6px 10px;">
-                                                        <a href="{{url('/xem') . '/' . $bttt[$i]->id}}"
-                                                           style="color: #333">
-                                                            <i class="fa fa-caret-right"
-                                                               aria-hidden="true"></i>
-                                                            {{$bttt[$i]->title}}
+                                                        style="padding: 6px 10px; text-align: justify">
+                                                        <a href="{{url('/xem') . '/' . $list->id}}"
+                                                        style="color: #333">
+                                                            <i class="fa fa-caret-right" aria-hidden="true"></i>
+                                                            {{$list->title}}
                                                         </a>
                                                     </li>
-                                                @endfor
+                                                @endforeach
                                             </ul>
 
                                         </div>
                                     </div>
                                 </div>
+
                             </div>
                         </div>
+                        <div class="clear"></div>
                     </div>
                 </div>
-                <div class="clear"></div>
-            </div>
-        </div>
-        <div>
-            <div>
-                <div>
-                    <div>
-                        <div>
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title"
-                                        style="font-size: 12px; font-weight: bold">
-                                        {{\App\Enums\PostType::VBCD_CDK}}
-                                    </h3>
-                                </div>
-                                <div class="panel-body" style="padding: 0px;">
-
-                                    <ul class="list-group"
-                                        style="margin-left: 0px; margin-bottom: 0px;">
-                                        @foreach($vbcd_dk as $vb)
-                                            <li class="list-group-item"
-                                                style="padding: 6px 10px; text-align: justify">
-                                                <a href="{{url('/xem') . '/' . $vb->id}}"
-                                                   style="color: #333">
-                                                    <i class="fa fa-caret-right" aria-hidden="true"></i>
-                                                    {{$vb->title}}
-                                                </a>
-                                            </li>
-                                        @endforeach
-                                    </ul>
-
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="clear"></div>
-            </div>
-        </div>
-        <div>
-            <div>
-                <div>
-                    <div>
-                        <div>
-                            <div class="panel panel-primary">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title"
-                                        style="font-size: 12px; font-weight: bold">
-                                        {{\App\Enums\PostType::VBCDDH}}
-                                    </h3>
-                                </div>
-                                <div class="panel-body" style="padding: 0px;">
-
-                                    <ul class="list-group"
-                                        style="margin-left: 0px; margin-bottom: 0px;">
-                                        @foreach($vbcddh as $vbc)
-                                            <li class="list-group-item"
-                                                style="padding: 6px 10px; text-align: justify">
-                                                <a href="{{url('/xem') . '/' . $vbc->id}}"
-                                                   style="color: #333">
-                                                    <i class="fa fa-caret-right" aria-hidden="true"></i>
-                                                    {{$vbc->title}}
-                                                </a>
-                                            </li>
-                                        @endforeach
-                                    </ul>
-
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="clear"></div>
-            </div>
-        </div>
+            @endif
+        @endforeach
     @endif
 
     <div>
