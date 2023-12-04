66af144b7713dc05bf631c6a1d9888ee801cfc2a
cuongnq <cuongnq211@gmail.com>
2023-12-05 01:30:38 +0700

    move GA credential path to env

--- config/analytics.php
+++ config/analytics.php
@@ -12,7 +12,7 @@ return [
      * to learn how to get this file. You can also pass the credentials as an array
      * instead of a file path.
      */
-    'service_account_credentials_json' => storage_path('app/analytics/pcttdaknong-credentials.json'),
+    'service_account_credentials_json' => storage_path(env('GA_CREDENTIAL')),
 
     /*
      * The amount of minutes the Google API responses will be cached.
